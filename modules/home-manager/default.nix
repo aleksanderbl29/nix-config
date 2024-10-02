c093b3fa97802e1787ddec6c3900205b91367f0c
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-10-02 17:09:47 +0200

    Fix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -61,7 +61,7 @@ in
         fu-commit = "git add flake.lock && git commit -m 'Update flake.lock'";
         open_nix = "nix-cd && code .";
         posit = "open -a 'Positron' .";
-        t7 = "cv '/Volumes/T7 Shield/developer'";
+        t7 = "cd '/Volumes/T7 Shield/developer'";
       };
       oh-my-zsh = {
         enable = true;
