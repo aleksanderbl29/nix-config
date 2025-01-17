aa6beb31270a13960a918aa06137d7580f9233d7
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2025-01-17 17:08:12 +0100

    Transfer some config from settings.json

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,7 +3,43 @@
     enable = true;
 
     userSettings = {
+      # Theme and icons
       "workbench.iconTheme" = "material-icon-theme";
+      "workbench.preferredLightColorTheme" = "Github Light Default";
+      "workbench.preferredDarkColorTheme" = "Github Dark Default";
+      "window.autoDetectColorScheme" = true;
+
+      # More visuals
+      "emptyIndent.highlightIndent" = true;
+      "editor.minimap.maxColumn" = 75;
+
+      # Smooth animations
+      "editor.smoothScrolling" = true;
+      "editor.cursorBlinking" = "smooth";
+      "editor.cursorSmoothCaretAnimation" = "on";
+      "workbench.list.smoothScrolling" = true;
+
+      # Editor
+      "editor.tabSize" = 2;
+
+      # Git
+      "git.enableSmartCommit" = true;
+      "git.confirmSync" = false;
+
+      # Copilot
+      "github.copilot.enable" = {
+        "*" = true;
+        "plaintext" = true;
+        "markdown" = false;
+      };
+      "github.copilot.editor.enableAutoCompletions" = true;
+
+      # Files
+      "files.associations" = {
+        "*.yml" = "yaml";
+        "*.yaml" = "yaml";
+      };
+      "r.plot.useHttpgd" = true;
     };
   };
 }
