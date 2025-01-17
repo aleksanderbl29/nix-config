fdd3e66723dcc459b7bde0c4c1439cc17336fe10
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2025-01-17 16:23:51 +0100

    Add default browser

--- machines/darwin/system.nix
+++ machines/darwin/system.nix
@@ -35,6 +35,7 @@
     NSNavPanelExpandedStateForSaveMode = true;
     # "com.apple.mouse.tapBehavior" = 1;
     ApplePressAndHoldEnabled = false;
+    LSPrefferedWebBroser = "app.zen-browser.zen";
   };
 
   system.defaults.CustomUserPreferences = {
