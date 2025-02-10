8c4adc472811f3d734b050a8cc2981fe4668124a
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-10 09:40:40 +0100

    Enable lfs

--- home/git.nix
+++ home/git.nix
@@ -4,6 +4,7 @@
     userEmail = "github@aleksanderbl.dk";
     userName = "aleksanderbl29";
     ignores = [ ".DS_Store" ];
+    lfs.enable = true;
     extraConfig = {
       push.autoSetupRemote = true;
       init.defaultBranch = "main";
