4965e45330294b23433bbc474d7f7aedc633d843
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-12 12:02:16 +0100

    Format with alejandra

--- flake.nix
+++ flake.nix
@@ -44,87 +44,103 @@
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
     my-nvf.url = "github:aleksanderbl29/nvf";
-
   };
 
-  outputs = inputs@{ self, nixpkgs, stable,
-    darwin, home-manager, nix-homebrew, homebrew-cask, homebrew-core, homebrew-bundle,
-    nix-vscode-extensions, nixos-hardware, my-nvf, ... }:
-  let
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    stable,
+    darwin,
+    home-manager,
+    nix-homebrew,
+    homebrew-cask,
+    homebrew-core,
+    homebrew-bundle,
+    nix-vscode-extensions,
+    nixos-hardware,
+    my-nvf,
+    ...
+  }: let
     m1 = "aarch64-darwin";
     rpi = "aarch64-linux";
     n100 = "x86_64-linux";
     mac_user = "aleksander";
     user = "aleksander";
 
-    myOverlays = [
-      inputs.nix-vscode-extensions.overlays.default
-    ];
+    myOverlays = [inputs.nix-vscode-extensions.overlays.default];
 
     # Configure nix-darwin machines
-    mkDarwinConfig = { system, hostname }: darwin.lib.darwinSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      pkgs = import nixpkgs {
+    mkDarwinConfig = {
+      system,
+      hostname,
+    }:
+      darwin.lib.darwinSystem {
         inherit system;
-        config = { allowUnfree = true; };
-        overlays = myOverlays;
+        specialArgs = {inherit inputs;};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+          overlays = myOverlays;
+        };
+        modules = [
+          ./machines/darwin # shared darwin config
+          ./machines/darwin/${hostname}.nix # machine-specific config
+
+          inputs.nix-homebrew.darwinModules.nix-homebrew
+
+          # Custom nvim config from nvf
+          {environment.systemPackages = [my-nvf.packages.${m1}.default];}
+
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {inherit (nixpkgs.config) allowUnfree;};
+              users.${mac_user} = {pkgs, ...}: {
+                nixpkgs.config.allowUnfree = true;
+                imports = [
+                  ./home # shared home-manager config for all machines
+                  ./home/darwin.nix # shared home-manager config for all darwin machines
+                  ./home/${hostname}.nix # machine-specific home-manager config
+                ];
+              };
+            };
+          }
+        ];
       };
-      modules = [
-        ./machines/darwin              # shared darwin config
-        ./machines/darwin/${hostname}.nix  # machine-specific config
-
-        inputs.nix-homebrew.darwinModules.nix-homebrew
-
-        # Custom nvim config from nvf
-        {environment.systemPackages = [ my-nvf.packages.${m1}.default ];}
-
-        home-manager.darwinModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {
-              inherit (nixpkgs.config) allowUnfree;
-            };
-            users.${mac_user} = { pkgs, ... }: {
-              nixpkgs.config.allowUnfree = true;
-              imports = [
-                ./home              # shared home-manager config for all machines
-                ./home/darwin.nix   # shared home-manager config for all darwin machines
-                ./home/${hostname}.nix  # machine-specific home-manager config
-              ];
-            };
-          };
-        }
-      ];
-    };
 
     # Configure nixos machines
-    mkNixosConfig = { system, hostname }: nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      pkgs = import nixpkgs {
+    mkNixosConfig = {
+      system,
+      hostname,
+    }:
+      nixpkgs.lib.nixosSystem {
         inherit system;
-        config = { allowUnfree = true; };
-        overlays = myOverlays;
-      };
-      modules = [
-        ./machines/nixos              # shared nixos config
-        ./machines/nixos/${hostname}.nix  # machine-specific config
+        specialArgs = {inherit inputs;};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+          overlays = myOverlays;
+        };
+        modules = [
+          ./machines/nixos # shared nixos config
+          ./machines/nixos/${hostname}.nix # machine-specific config
 
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${user} = { pkgs, ... }: {
-              imports = [
-                ./home    # same shared home-manager config
-              ];
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${user} = {pkgs, ...}: {
+                imports = [
+                  ./home # same shared home-manager config
+                ];
+              };
             };
-          };
-        }
-      ];
-    };
+          }
+        ];
+      };
   in {
     darwinConfigurations = {
       aleks-old-mbp = mkDarwinConfig {
