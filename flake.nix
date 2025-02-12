70f8d462fb2697840fa1a76fabadd01e166a100b
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-12 12:12:54 +0100

    Revert "Format things with alejandra"
    This reverts commit 44cce15678c9999c2302afa4c1d66d1b52ab0763.

--- flake.nix
+++ flake.nix
@@ -46,138 +46,125 @@
     my-nvf.url = "github:aleksanderbl29/nvf";
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      stable,
-      darwin,
-      home-manager,
-      nix-homebrew,
-      homebrew-cask,
-      homebrew-core,
-      homebrew-bundle,
-      nix-vscode-extensions,
-      nixos-hardware,
-      my-nvf,
-      ...
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    stable,
+    darwin,
+    home-manager,
+    nix-homebrew,
+    homebrew-cask,
+    homebrew-core,
+    homebrew-bundle,
+    nix-vscode-extensions,
+    nixos-hardware,
+    my-nvf,
+    ...
+  }: let
+    m1 = "aarch64-darwin";
+    rpi = "aarch64-linux";
+    n100 = "x86_64-linux";
+    mac_user = "aleksander";
+    user = "aleksander";
+
+    myOverlays = [inputs.nix-vscode-extensions.overlays.default];
+
+    # Configure nix-darwin machines
+    mkDarwinConfig = {
+      system,
+      hostname,
     }:
-    let
-      m1 = "aarch64-darwin";
-      rpi = "aarch64-linux";
-      n100 = "x86_64-linux";
-      mac_user = "aleksander";
-      user = "aleksander";
-
-      myOverlays = [ inputs.nix-vscode-extensions.overlays.default ];
-
-      # Configure nix-darwin machines
-      mkDarwinConfig =
-        {
-          system,
-          hostname,
-        }:
-        darwin.lib.darwinSystem {
+      darwin.lib.darwinSystem {
+        inherit system;
+        specialArgs = {inherit inputs;};
+        pkgs = import nixpkgs {
           inherit system;
-          specialArgs = { inherit inputs; };
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-            };
-            overlays = myOverlays;
-          };
-          modules = [
-            ./machines/darwin # shared darwin config
-            ./machines/darwin/${hostname}.nix # machine-specific config
+          config = {allowUnfree = true;};
+          overlays = myOverlays;
+        };
+        modules = [
+          ./machines/darwin # shared darwin config
+          ./machines/darwin/${hostname}.nix # machine-specific config
 
-            inputs.nix-homebrew.darwinModules.nix-homebrew
+          inputs.nix-homebrew.darwinModules.nix-homebrew
 
-            # Custom nvim config from nvf
-            { environment.systemPackages = [ my-nvf.packages.${m1}.default ]; }
+          # Custom nvim config from nvf
+          {environment.systemPackages = [my-nvf.packages.${m1}.default];}
 
-            home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit (nixpkgs.config) allowUnfree; };
-                users.${mac_user} =
-                  { pkgs, ... }:
-                  {
-                    nixpkgs.config.allowUnfree = true;
-                    imports = [
-                      ./home # shared home-manager config for all machines
-                      ./home/darwin.nix # shared home-manager config for all darwin machines
-                      ./home/${hostname}.nix # machine-specific home-manager config
-                    ];
-                  };
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {inherit (nixpkgs.config) allowUnfree;};
+              users.${mac_user} = {pkgs, ...}: {
+                nixpkgs.config.allowUnfree = true;
+                imports = [
+                  ./home # shared home-manager config for all machines
+                  ./home/darwin.nix # shared home-manager config for all darwin machines
+                  ./home/${hostname}.nix # machine-specific home-manager config
+                ];
               };
-            }
-          ];
-        };
-
-      # Configure nixos machines
-      mkNixosConfig =
-        {
-          system,
-          hostname,
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs; };
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
             };
-            overlays = myOverlays;
-          };
-          modules = [
-            ./machines/nixos # shared nixos config
-            ./machines/nixos/${hostname}.nix # machine-specific config
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.${user} =
-                  { pkgs, ... }:
-                  {
-                    imports = [
-                      ./home # same shared home-manager config
-                    ];
-                  };
-              };
-            }
-          ];
-        };
-    in
-    {
-      darwinConfigurations = {
-        aleks-old-mbp = mkDarwinConfig {
-          system = m1;
-          hostname = "aleks-old-mbp";
-        };
-        aleks-mbp = mkDarwinConfig {
-          system = m1;
-          hostname = "aleks-mbp";
-        };
-        aleks-air = mkDarwinConfig {
-          system = "x86_64-darwin";
-          hostname = "aleks-air";
-        };
+          }
+        ];
       };
 
-      #   # n100 = mkNixosConfig {
-      #   #   system = n100;
-      #   #   hostname = "n100";
-      #   # };
-      #   hyperhdr = mkNixosConfig {
-      #     system = rpi;
-      #     hostname = "hyperhdr";
-      #   };
-      # };
+    # Configure nixos machines
+    mkNixosConfig = {
+      system,
+      hostname,
+    }:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit inputs;};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+          overlays = myOverlays;
+        };
+        modules = [
+          ./machines/nixos # shared nixos config
+          ./machines/nixos/${hostname}.nix # machine-specific config
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${user} = {pkgs, ...}: {
+                imports = [
+                  ./home # same shared home-manager config
+                ];
+              };
+            };
+          }
+        ];
+      };
+  in {
+    darwinConfigurations = {
+      aleks-old-mbp = mkDarwinConfig {
+        system = m1;
+        hostname = "aleks-old-mbp";
+      };
+      aleks-mbp = mkDarwinConfig {
+        system = m1;
+        hostname = "aleks-mbp";
+      };
+      aleks-air = mkDarwinConfig {
+        system = "x86_64-darwin";
+        hostname = "aleks-air";
+      };
     };
+
+    #   # n100 = mkNixosConfig {
+    #   #   system = n100;
+    #   #   hostname = "n100";
+    #   # };
+    #   hyperhdr = mkNixosConfig {
+    #     system = rpi;
+    #     hostname = "hyperhdr";
+    #   };
+    # };
+  };
 }
