691eab5992cf8a9b548abb8bf7a32682b8172d98
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-16 19:05:29 +0100

    Move gnome config

--- machines/nixos/nixos-1/default.nix
+++ machines/nixos/nixos-1/default.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./gnome.nix
     ];
 
   # Bootloader.
@@ -44,13 +45,6 @@
     LC_TELEPHONE = "da_DK.UTF-8";
     LC_TIME = "da_DK.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
---
+++ machines/nixos/nixos-1/gnome.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  # minimized for clarity.
+  # Some of these might not be needed. After some trial and error
+  # I got this working with these configs.
+  # I do not have the patience to rn an elimination test.
+
+  services.gnome.gnome-remote-desktop.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome3.gnome-session}/bin/gnome-session";
+  services.xrdp.openFirewall = true;
+
+  environment.systemPackages = with pkgs; [
+    gnome3.gnome-session
+  ];
+
+  # Open ports in the firewall.
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 3389 ];
+    allowedUDPPorts = [ 3389 ];
+  };
+}
