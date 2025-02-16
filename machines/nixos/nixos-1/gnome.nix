9a8658657195506875fc22befee82ada898b817b
aleksanderbl29 <github@aleksanderbl.dk>
2025-02-16 19:06:36 +0100

    Update package name

--- machines/nixos/nixos-1/gnome.nix
+++ machines/nixos/nixos-1/gnome.nix
@@ -13,11 +13,11 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome3.gnome-session}/bin/gnome-session";
+  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
   services.xrdp.openFirewall = true;
 
   environment.systemPackages = with pkgs; [
-    gnome3.gnome-session
+    gnome-session
   ];
 
   # Open ports in the firewall.
