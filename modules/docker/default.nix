46ff5a7cdfbfc389ebd0ef80d5e8271f881f6334
aleksanderbl29 <github@aleksanderbl.dk>
2025-04-05 17:25:43 +0200

    add pihole

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -11,6 +11,8 @@ in
   imports = [
     # ./traefik.nix
     ./beszel.nix
+    ./littlelink.nix
+    ./pihole.nix
   ];
 
   options.docker = {
@@ -30,6 +32,28 @@ in
         backend = "docker";
       };
     };
-  };
 
+    # Create required Docker networks
+    systemd.services.docker-networks = {
+      description = "Create Docker networks";
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+      wantedBy = [ "docker.service" ];
+      after = [ "docker.service" ];
+      path = [ config.virtualisation.docker.package ];
+      script = ''
+        # Create the 'proxy' network if it doesn't exist
+        if ! docker network ls | grep -q proxy; then
+          docker network create proxy
+        fi
+
+        # Create the 'external' network if it doesn't exist
+        if ! docker network ls | grep -q external; then
+          docker network create external
+        fi
+      '';
+    };
+  };
 }
