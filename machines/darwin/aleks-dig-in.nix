2da68c5f7efd05fe1ca096ad23d0c80ecf9cadc6
aleksander-digin <aleksander@dig-in.dk>
2025-06-10 08:49:02 +0200

    Import the rest of the options

--- machines/darwin/aleks-dig-in.nix
+++ machines/darwin/aleks-dig-in.nix
@@ -1,10 +1,12 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
   imports = [
     # ./work/default.nix
+    ../common/darwin/system.nix
   ];
 
   environment = {
@@ -13,15 +15,15 @@
     ];
   };
 
-  system.keyboard = {
+  system.keyboard = lib.mkForce {
     enableKeyMapping = true;
     remapCapsLockToControl = true;
   };
 
-  system.defaults = {
+  system.defaults = lib.mkForce {
     loginwindow.LoginwindowText = ''
-      Tilhører Tænketanken for Digital Infrastruktur
-
+      Tilhører 
+      Tænketanken for Digital Infrastruktur
 
     '';
   };
