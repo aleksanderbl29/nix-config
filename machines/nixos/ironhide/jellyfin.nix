fe2323d59a2cd0577972f6df319d2e823f97df9f
aleksanderbl29 <github@aleksanderbl.dk>
2025-06-13 10:34:38 +0200

    Format

--- machines/nixos/ironhide/jellyfin.nix
+++ machines/nixos/ironhide/jellyfin.nix
@@ -2,29 +2,26 @@
 
 {
   nixpkgs.overlays = with pkgs; [
-    (
-      final: prev: {
-        vaapiIntel = prev.vaapiIntel.override { enableHybridCodec = true; };
-      }
-    )
-    (
-      final: prev:
-        {
-          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
-            installPhase = ''
-              runHook preInstall
+    (final: prev: {
+      vaapiIntel = prev.vaapiIntel.override { enableHybridCodec = true; };
+    })
+    (final: prev: {
+      jellyfin-web = prev.jellyfin-web.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          installPhase = ''
+            runHook preInstall
 
-              # this is the important line
-              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+            # this is the important line
+            sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
 
-              mkdir -p $out/share
-              cp -a dist $out/share/jellyfin-web
+            mkdir -p $out/share
+            cp -a dist $out/share/jellyfin-web
 
-              runHook postInstall
-            '';
-          });
+            runHook postInstall
+          '';
         }
-    )
+      );
+    })
   ];
 
   # Hardware configuration for Intel N100
