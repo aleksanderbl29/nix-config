9fbb7e334db37b73ddc55439c0e79e4dcce1ebe6
aleksanderbl29 <github@aleksanderbl.dk>
2025-11-06 09:06:37 +0100

    Add nix-format alias

--- home/zsh.nix
+++ home/zsh.nix
@@ -86,6 +86,7 @@
         fu-commit = "nix flake update && git add flake.lock && git commit -m 'Update flake.lock'";
         open-nix = "nix-cd && code .";
         nix-nvim = "nix-cd && nvim .";
+        nix-format = "nix run nixpkgs#nixfmt-rfc-style **/*.nix";
 
         # cd shortcuts
         ".." = "cd ..";
