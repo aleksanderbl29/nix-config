e17853861ad5a098072dadabe74e2198b5cbc679
aleksanderbl29 <github@aleksanderbl.dk>
2026-01-03 11:21:25 +0100

    fix(gatus): change interval

--- machines/nixos/nix-proxy-1/status.nix
+++ machines/nixos/nix-proxy-1/status.nix
@@ -7,28 +7,28 @@
         name = "Website";
         group = "Portfolio";
         url = "https://aleksanderbl.dk";
-        interval = "30s";
+        interval = "5m";
         conditions = [ "[STATUS] == 200" ];
       }
       {
         name = "Dev website";
         group = "Portfolio";
         url = "https://dev.aleksanderbl.dk";
-        interval = "30s";
+        interval = "5m";
         conditions = [ "[STATUS] == 200" ];
       }
       {
         name = "Website API";
         group = "Portfolio";
         url = "https://api.aleksanderbl.dk";
-        interval = "30s";
+        interval = "5m";
         conditions = [ "[STATUS] == 200" ];
       }
       {
         name = "Hvem Vinder Valget?";
         group = "Side Projects";
         url = "https://hvemvindervalget.dk";
-        interval = "30s";
+        interval = "5m";
         conditions = [ "[STATUS] == 200" ];
       }
     ];
