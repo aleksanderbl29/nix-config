48e3383a7b4e74b556e81331af95a8b268f8c539
aleksanderbl29 <github@aleksanderbl.dk>
2026-01-09 17:42:26 +0100

    feat(caddy): add home assistant proxy for tailscale network

--- homelab/caddy/external-proxies.nix
+++ homelab/caddy/external-proxies.nix
@@ -1,9 +1,12 @@
 { lib, config, ... }:
 let
   cfg = config.homelab;
+  hasEnabledServices = lib.any (service: (builtins.isAttrs service) && (service.enable or false)) (
+    lib.attrValues cfg.services
+  );
 in
 {
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf (cfg.enable || hasEnabledServices) {
     services.caddy.virtualHosts = lib.mkMerge [
       # External service reverse proxies
       {
@@ -11,6 +14,19 @@ in
           extraConfig = ''
             tls ${cfg.tls.certFile} ${cfg.tls.keyFile}
             reverse_proxy http://hyperhdr:8090
+          '';
+        };
+      }
+      {
+        "ha.${cfg.baseDomain}" = {
+          extraConfig = ''
+            tls ${cfg.tls.certFile} ${cfg.tls.keyFile}
+            reverse_proxy http://homeassistant:8123 {
+              header_up X-Forwarded-Proto {scheme}
+              header_up X-Forwarded-Host {host}
+              header_up X-Forwarded-For {remote_host}
+              header_up X-Real-IP {remote_host}
+            }
           '';
         };
       }
