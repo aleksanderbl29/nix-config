ff6a07e85502efd9f3ba87e30ccd7b644dc47197
aleksanderbl29 <github@aleksanderbl.dk>
2026-02-12 21:09:24 +0100

    feat(disko): add disko flake input

--- flake.nix
+++ flake.nix
@@ -48,6 +48,11 @@
       url = "github:aleksanderbl29/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
