b59de833a8c7a4e4b8fe2642d6f0269b481e65d6
aleksanderbl29 <github@aleksanderbl.dk>
2026-04-30 09:49:22 +0200

    feat(cache): add some caches

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -21,10 +21,16 @@
       substituters = [
         "https://cache.nixos.org"
         "https://aleksanderbl.cachix.org"
+        "https://nvf.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "aleksanderbl.cachix.org-1:qt6zlsWxJ5XNVmSGnhPuMzXKBiY02pQIzLTCSK42hRM="
+        "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
   };
