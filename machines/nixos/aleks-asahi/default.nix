d8d257ca0805cff83cdcaa81243adb8a94b59af8
aleksanderbl29 <github@aleksanderbl.dk>
2026-09-03 14:42:27 +0200

    feat(pkgs): add codex and cursor-agent

--- machines/nixos/aleks-asahi/default.nix
+++ machines/nixos/aleks-asahi/default.nix
@@ -65,15 +65,20 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    ghostty
-    git
-    gh
-    vscode-fhs
-    yazi
-    obsidian
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      ghostty
+      git
+      gh
+      vscode-fhs
+      yazi
+      obsidian
+    ])
+    ++ (with inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}; [
+      codex
+      cursor-agent
+    ]);
 
   programs.firefox.enable = true;
 
